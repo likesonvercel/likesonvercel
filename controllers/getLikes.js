ba03e579c4ae7205763a7f2a4246a4cdfa289b06
Sambhav Saxena <sambhavsaxena02@gmail.com>
2023-07-07 20:40:21 +0530

    hehe

--- controllers/getLikes.js
+++ controllers/getLikes.js
@@ -2,7 +2,7 @@ import asyncHandler from "express-async-handler";
 import blogPosts from "../data.js";
 
 const getLikes = asyncHandler(async (req, res) => {
-  const { title } = req.params;
+  const { title } = req.query;
   let { likes } = blogPosts.find((post) => post.title === title);
   return res.json({ likes });
 });
